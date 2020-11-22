f696efd442c30f9affcc0eeb002cef099bebb829
Nathan Hadi <nhadi3@illinois.edu>
2020-11-22 04:16:57 -0600

    Most of backend and frontend connected.

--- project.py
+++ project.py
@@ -1,27 +1,71 @@
 from flask import Flask, render_template, redirect, url_for, request
+import backend as back
+import reminder
 
 app = Flask(__name__)
 
-import backend
-import reminder
+user = "testing"
 
 @app.route('/')
 @app.route('/home')
 def home_page():
-    return render_template('home.html')
+    # Get list of user readings from the database.
+    user_readings = back.get_readings(user)
+    return render_template('home.html', readings=user_readings)
 
-@app.route('/goal')
+@app.route('/add-goal', methods=['GET', 'POST'])
 def goal_page():
+    if request.method == 'POST':
+        # Get all necessary data for adding a goal.
+        name = request.form.get("name", None)
+        start_date = request.form.get("start_date", None)
+        end_date = request.form.get("end_date", None)
+        amount = request.form.get("amount", None)
+        option = request.form.get("options", None)
+        dates = request.form.getlist('date', None)
+        # print("Goal Name:", name)
+        # print("Start Date:", start_date)
+        # print("End Date:", end_date)
+        # print("Amount:", amount)
+        # print("Option:", option)
+        # print("Dates:", dates)
+        return redirect(url_for('home_page'))
     return render_template('add-goal.html')
 
-@app.route('/add-reading')
+@app.route('/add-reading', methods=['GET', 'POST'])
 def add_page():
+    if request.method == 'POST':
+        # Get all necessary data for adding a reading.
+        name = request.form.get("name", None)
+        pages = request.form.get("pages", None)
+        chapter = request.form.get("chapter", None)
+        tags = request.form.get("tags", None)
+        # Add the reading to the database.
+        back.add_reading(user, name, pages, chapter, None, None)
+        return redirect(url_for('home_page'))
     return render_template('add-reading.html')
 
-@app.route('/edit-reading')
+@app.route('/edit-reading', methods=['GET', 'POST'])
 def edit_page():
+    if request.method == 'POST':
+        # Get all necessary data for editing a reading.
+        name = request.form.get("name", None)
+        pages = request.form.get("pages", None)
+        chapter = request.form.get("chapter", None)
+        tags = request.form.get("tags", None)
+        print("Name:", name)
+        print("Pages:", pages)
+        print("Chapter", chapter)
+        print("Tags:", tags)
     return render_template('edit-reading.html')
 
-@app.route('/settings')
+@app.route('/settings', methods=['GET', 'POST'])
 def settings_page():
+    if request.method == 'POST':
+        setting = request.form.get("setting", None)
+        times = request.form.get("num_times", None)
+        frequency = request.form.get("frequency", None)
+        print(setting)
+        print(times)
+        print(frequency)
     return render_template('settings.html')
